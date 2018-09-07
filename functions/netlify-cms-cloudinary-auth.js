8d29ce4aa701c9b245140a7c1715e100913b0e3c
Shawn Erquhart <shawn@erquh.art>
2018-09-07 17:55:41 -0400

    update test function

--- functions/netlify-cms-cloudinary-auth.js
+++ functions/netlify-cms-cloudinary-auth.js
@@ -1,9 +1,12 @@
+import hasha from 'hasha';
+
 exports.handler = async (event, context) => {
+  const { CLOUDINARY_USERNAME: username, CLOUDINARY_API_SECRET: secret } = process.env
+  const timestamp = Date.now()
+  const str = `cloud_name=erquhart&timestamp=${timestamp}&username=${username}${secret}`
+  const hash = hasha(str, { algorithm: 'sha256' });
   return {
     statusCode: 200,
-    body: `
-      ${JSON.stringify(event, null, 2)}
-      ${JSON.stringify(context, null, 2)}
-    `,
+    body: hash,
   }
 }
