2f315dafc9409822303250f804f85b2b4d6a639a
Shawn Erquhart <shawn@erquh.art>
2018-09-07 17:34:51 -0400

    update test function

--- functions/netlify-cms-cloudinary-auth.js
+++ functions/netlify-cms-cloudinary-auth.js
@@ -1,9 +1,6 @@
 exports.handler = function(event, context, callback) {
   return {
     statusCode: 200,
-    body: `
-      ${JSON.stringify(event, null, 2)}
-      ${JSON.stringify(context, null, 2)}
-    `,
+    body: "test",
   };
 }
