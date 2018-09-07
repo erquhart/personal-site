0286cc02a807a83cc6aca51cda96c02c00026c78
Shawn Erquhart <shawn@erquh.art>
2018-09-07 18:15:45 -0400

    add functions build task

--- functions/netlify-cms-cloudinary-auth.js
+++ functions/netlify-cms-cloudinary-auth.js
@@ -1,13 +1,210 @@
-const hasha = require('hasha');
+(function(e, a) { for(var i in a) e[i] = a[i]; }(exports, /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
 
-exports.handler = async (event, context) => {
-  // test
-  const { CLOUDINARY_USERNAME: username, CLOUDINARY_API_SECRET: secret } = process.env
-  const timestamp = Date.now()
-  const str = `cloud_name=erquhart&timestamp=${timestamp}&username=${username}${secret}`
-  const hash = hasha(str, { algorithm: 'sha256' });
-  return {
-    statusCode: 200,
-    body: hash,
-  }
-}
+"use strict";
+
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+const hasha = __webpack_require__(1);
+
+exports.handler = (() => {
+  var _ref = _asyncToGenerator(function* (event, context) {
+    // test
+    const { CLOUDINARY_USERNAME: username, CLOUDINARY_API_SECRET: secret } = process.env;
+    const timestamp = Date.now();
+    const str = `cloud_name=erquhart&timestamp=${timestamp}&username=${username}${secret}`;
+    const hash = hasha(str, { algorithm: 'sha256' });
+    return {
+      statusCode: 200,
+      body: hash
+    };
+  });
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+})();
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+const fs = __webpack_require__(2);
+const crypto = __webpack_require__(3);
+const isStream = __webpack_require__(4);
+
+const hasha = (input, opts) => {
+	opts = opts || {};
+
+	let outputEncoding = opts.encoding || 'hex';
+
+	if (outputEncoding === 'buffer') {
+		outputEncoding = undefined;
+	}
+
+	const hash = crypto.createHash(opts.algorithm || 'sha512');
+
+	const update = buf => {
+		const inputEncoding = typeof buf === 'string' ? 'utf8' : undefined;
+		hash.update(buf, inputEncoding);
+	};
+
+	if (Array.isArray(input)) {
+		input.forEach(update);
+	} else {
+		update(input);
+	}
+
+	return hash.digest(outputEncoding);
+};
+
+hasha.stream = opts => {
+	opts = opts || {};
+
+	let outputEncoding = opts.encoding || 'hex';
+
+	if (outputEncoding === 'buffer') {
+		outputEncoding = undefined;
+	}
+
+	const stream = crypto.createHash(opts.algorithm || 'sha512');
+	stream.setEncoding(outputEncoding);
+	return stream;
+};
+
+hasha.fromStream = (stream, opts) => {
+	if (!isStream(stream)) {
+		return Promise.reject(new TypeError('Expected a stream'));
+	}
+
+	opts = opts || {};
+
+	return new Promise((resolve, reject) => {
+		stream
+			.on('error', reject)
+			.pipe(hasha.stream(opts))
+			.on('error', reject)
+			.on('finish', function () {
+				resolve(this.read());
+			});
+	});
+};
+
+hasha.fromFile = (fp, opts) => hasha.fromStream(fs.createReadStream(fp), opts);
+
+hasha.fromFileSync = (fp, opts) => hasha(fs.readFileSync(fp), opts);
+
+module.exports = hasha;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = require("fs");
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = require("crypto");
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var isStream = module.exports = function (stream) {
+	return stream !== null && typeof stream === 'object' && typeof stream.pipe === 'function';
+};
+
+isStream.writable = function (stream) {
+	return isStream(stream) && stream.writable !== false && typeof stream._write === 'function' && typeof stream._writableState === 'object';
+};
+
+isStream.readable = function (stream) {
+	return isStream(stream) && stream.readable !== false && typeof stream._read === 'function' && typeof stream._readableState === 'object';
+};
+
+isStream.duplex = function (stream) {
+	return isStream.writable(stream) && isStream.readable(stream);
+};
+
+isStream.transform = function (stream) {
+	return isStream.duplex(stream) && typeof stream._transform === 'function' && typeof stream._transformState === 'object';
+};
+
+
+/***/ })
+/******/ ])));
---
+++ src/lambda/netlify-cms-cloudinary-auth.js
@@ -0,0 +1,13 @@
+const hasha = require('hasha');
+
+exports.handler = async (event, context) => {
+  // test
+  const { CLOUDINARY_USERNAME: username, CLOUDINARY_API_SECRET: secret } = process.env
+  const timestamp = Date.now()
+  const str = `cloud_name=erquhart&timestamp=${timestamp}&username=${username}${secret}`
+  const hash = hasha(str, { algorithm: 'sha256' });
+  return {
+    statusCode: 200,
+    body: hash,
+  }
+}
